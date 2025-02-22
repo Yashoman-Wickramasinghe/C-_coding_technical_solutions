b93c9ec8fed2c1d4ef60fd50f68fcaf72e577bf7
Yasho Wickramasinghe <yashoman0608@gmail.com>
2025-02-22 17:00:27 +1100

    Generics and Arrays vs ArrayLists sample coding. Generic class called "BoxGenerics" SortedLists and SortedDictionries incomplete.

---
+++ reverseString/BoxGeneric.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseString
+{
+    public class BoxGeneric<T>
+    {
+        private T item;
+
+        public void SetItem(T value)
+        {
+            item = value;
+        }
+
+        public T GetItem()
+        {
+            return item;
+        }
+    }
+}
